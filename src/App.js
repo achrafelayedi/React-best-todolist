f5d0dd6c70601dc0099cb89a039da80aa1976b63
EL AYEDI Achraf <achraf.elayedi@coexya.eu>
2022-09-12 16:42:59 +0200

    add html and css + decorating

--- src/App.js
+++ src/App.js
@@ -5,9 +5,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function App() {
   return (
     <div className="App">
+      <h1>What is it today?</h1>
+
+      <form autoComplete='off'>
+        <input type="text" name="todos" id="todos" required placeholder='Add me to your todos please!' />
+
+        <button type="submit" class="btn btn-primary">
+          <i class="fa fa-check" aria-hidden="true"></i>
+          Add
+        </button>      
+      </form>
+
+      <ul>
+        <li>
+          <label htmlFor=''>
+            <input type="checkbox" id="" />
+            Coocking to do
+          </label>
+          <button>
+            Edit
+          </button>
+        </li>
+        <li>
+          <label htmlFor='' >
+            <input type="checkbox" id="" />
+            Coocking to do
+          </label>
+          <button>
+            Edit
+          </button>
+        </li>
+      </ul>
+
+      <div className='row'>
+        <label>
+          <input type="checkbox" name="all" id="all"/>
+          All
+        </label>
+        <p>
+          You have 0 to do
+        </p>
+        <i class="fa fa-trash-o" aria-hidden="true"></i>
+        <button id='delete'>
+          Delete
+        </button>
+      </div>
       {/* <i class="fa fa-pencil-square-o" aria-hidden="true"></i> */}
     </div>
-  );
+  ); 
 }
 
 export default App;
